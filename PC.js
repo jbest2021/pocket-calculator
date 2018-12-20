5c3e8476c6b3ccc98e5efc726d43cb07f2586ab9
Jordan Best <jbest@ucvts.org>
2018-12-20 13:11:02 -0500

    initial commit

---
+++ PC.js
@@ -0,0 +1,123 @@
+function digit0() {
+
+}
+
+
+function digit1() {
+
+}
+
+
+function digit1() {
+
+}
+
+
+function digit2() {
+
+}
+
+
+function digit3() {
+
+}
+
+
+function digit4() {
+
+}
+
+
+function digit5() {
+
+}
+
+
+function digit6() {
+
+}
+
+
+function digit7() {
+
+}
+
+
+function digit8() {
+
+}
+
+
+function digit9() {
+
+}
+
+
+function decimal() {
+
+}
+
+
+function division() {
+
+}
+
+
+function multipllication() {
+
+}
+
+
+function subtraction() {
+
+}
+
+
+function addition() {
+
+}
+
+
+function equals() {
+
+}
+
+
+function percentage() {
+
+}
+
+
+function negation() {
+
+}
+
+
+function clear() {
+
+}
+
+
+function powers() {
+
+}
+
+
+function roots() {
+
+}
+
+
+function factorial() {
+
+}
+
+
+function constants() {
+
+}
+
+
+function trigonometry() {
+
+}
